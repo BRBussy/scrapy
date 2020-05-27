2216bef4beb9c43dfa839a16839e7235f6163d36
Bernard Bussy <brbitzbussy@gmail.com>
2020-05-27 17:35:10 +0200

    separate process page func

--- tut/spiders/quotes_spider.py
+++ tut/spiders/quotes_spider.py
@@ -16,12 +16,19 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         product_links = set()
-        print('__________')
+
         for item in response.xpath('//div[@class="shop-container"]/div[contains(@class, "products")]/div'):
             for link in Selector(text=item.extract()).xpath('//a/@href'):
                 link_text = link.extract()
                 if 'http' in link_text and 'product' in link_text:
                     product_links.add(link.extract())
+
         for link in product_links:
-            print(link)
-        print('__________')
+            yield scrapy.Request(
+                url=link,
+                callback=self.process_product_page,
+                cookies={'age_gate': 21, 'tk_ai': 'woo:dBlQRi3iIybLcJZIZaok+wyL'}
+            )
+
+    def process_product_page(self, response):
+        print('process page!')
